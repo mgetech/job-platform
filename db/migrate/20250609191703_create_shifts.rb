aa6f0d9fc1a5e64beda087980a4d7af0a2426103
Majid G.E <mgetech@yahoo.com>
2025-06-09 21:19:59 +0200

    feat(db): Add tables for job listings, languages, and shifts

---
+++ db/migrate/20250609191703_create_shifts.rb
@@ -0,0 +1,11 @@
+class CreateShifts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :shifts do |t|
+      t.references :job, null: false, foreign_key: true
+      t.datetime :start_time, null: false
+      t.datetime :end_time, null: false
+
+      t.timestamps
+    end
+  end
+end
